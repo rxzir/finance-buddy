41f6c0bb31870947177f782021b9f505161ebacb
Razi <rxzirr@gmail.com>
2026-07-09 00:13:42 +0800

    Initial Commit

---
+++ Financial buddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Financial buddy
+//
+//  Created by Razi on 09/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Financial buddy/Financial_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_buddyApp.swift
+//  Financial buddy
+//
+//  Created by Razi on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Financial_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
